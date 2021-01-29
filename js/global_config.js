c1fecaf678addc326025804e860ff3b76ef7cc00
Lu Yihui <lu_yihui@126.com>
2021-01-29 18:09:45 +0800

    白色底

--- js/global_config.js
+++ js/global_config.js
@@ -5,9 +5,10 @@ var fontSize_all_legend = 18
 // 坐标轴字体大小
 var fontSize_axis_label = 16
 // 坐标轴颜色，白色
-var color_axis = '#fff'
+// var color_axis = '#fff'
+var color_axis = '#000'
 // 图例颜色，白色
-var color_legend = '#fff'
+var color_legend = '#000'
 // echart toolbox字体大小
 var echart_toolbox_size = 18
 // 折线图线宽
